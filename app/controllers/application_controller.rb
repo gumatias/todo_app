13d4fef543990e82b9e672caefcca51300edaa2d
Gustavo Matias <gustavo@matias.com>
2016-02-02 04:52:10 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def  aaa
-      1 + 1
+  def  aaa (a, b   , c,d,e,f    ,    g)
+        1 + 1
+        puts "asdfas dfas dfas dfasd fasd fasdf asd fasd fasd fasd asd fasdf asdfas dfas dfas dfasd fasd fasd fasd f"
+      { one: 1, two: 2 }
+  case
+    when 1 == 2
+      puts 'Not again!'
+    end
   end
+  
+  def something()
+  end
+  
+  def 1 + 1; end                                    
+  
+  
 end
+
+
+
